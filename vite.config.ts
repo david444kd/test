eec5d70a1df268834de01a02647e1105e9ed202b
david444kd <dkozahmetov132@gmail.com>
2024-11-16 21:26:05 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
 
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve("./src"),
     },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
   },
 });
